150d473b55a17d16feda47dd9e5037d6ad574dae
wangli147 <1475986800@qq.com>
2017-07-31 08:08:44 +0800

    111

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import { Carousel } from 'antd';
 import './App.css';
 import $ from 'jquery';
 import Home from './js/Home';
-import Enterprise from './Enterprise';
+import Enterprise from './js/Enterprise';
+import Finance from './js/Finance';
+import AboutMe from './js/AboutMe';
+import News from './js/News';
 
 import logo from './imgs/logo.png';
 import logos from './imgs/logo2.png';
@@ -15,7 +18,26 @@ import {
 } from 'react-router-dom'
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state={
+      nav:[]
+    }
+  }
   componentDidMount(){
+    $.ajax({
+        type:"post",
+        url:"http://localhost:8005/home/nav",
+        async:true,
+        contentType:false,
+        processData:false,
+        success:function(e){
+          this.setState({
+            nav:e
+          })        
+          console.log(e)
+            }
+      });
   	$(function(){
   		$('.wl_nav li').click(function(){
   			$(this).addClass('active').siblings().removeClass('active');
@@ -31,8 +53,18 @@ class App extends Component {
                 <div className="wl_merge clear">
                     <Link to="/"  className="wl_logo left"><img src={logo} alt="logo"/></Link> 
                      <ul className="right wl_nav">
-                        <li className="active"><Link to="/">首页</Link></li>
-                        <li><Link to="/Enterprise">企业金融</Link></li>
+                     {this.state.nav.map(function(e,i){
+                        return(
+                            <div>
+                                <li className="active"><Link to="/">首页</Link></li>
+                                <li><Link to="/Enterprise">企业金融</Link></li>
+                                <li><Link to="/Finance">个人金融</Link></li>
+                                <li><Link to="/AboutMe">关于我们</Link></li>
+                                <li><Link to="/News">公告新闻</Link></li>
+                            </div>
+                        )
+                     })}
+                        
                      </ul>          
                 </div>  
             </div>
@@ -42,7 +74,9 @@ class App extends Component {
                      <ul className="right wl_nav">
                         <li className="active"><Link to="/">首页</Link></li>
                         <li><Link to="/Enterprise">企业金融</Link></li>
-                        
+                         <li><Link to="/Finance">个人金融</Link></li>
+                        <li><Link to="/AboutMe">关于我们</Link></li>
+                        <li><Link to="/News">公告新闻</Link></li>
                      </ul>          
                 </div>  
             </div>
@@ -51,8 +85,9 @@ class App extends Component {
 
 
           <Route path="/Enterprise" component={Enterprise}/>
-
-          
+          <Route path="/Finance" component={Finance}/>
+          <Route path="/AboutMe" component={AboutMe}/>
+          <Route path="/News" component={News}/>
           
          
       </div>
